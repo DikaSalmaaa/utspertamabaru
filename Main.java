2fcb077908be54117f7f94c2489cde4619b71465
DikaSalmaaa <salmalailidika@gmail.com>
2023-05-11 16:32:20 +0700

    menjawabuts

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+import pengguna.User;
+import pengguna.Mhs;
+
 class Main {
     public static void main(String[] args) {
-        //Menambah Komentar Baru
+        
+        Mhs mhs = new Mhs("username", "password");
 
+        mhs.setNama("Dika Salma Laili");
+        mhs.setNim("G111200046");
+        mhs.setSmt(6);
+
+        System.out.println("Nama Mahasiswa: " + mhs.getNama());
+        System.out.println("Nim Mhs: " + mhs.getNim());
+        System.out.println("Smt Mhs: " + mhs.getSmt());
+        System.out.println("USERNAME: " + mhs.getUsername());
+        System.out.println("PASSWORD: " + mhs.getPassword());
     }
 }
